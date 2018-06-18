944bddb56d8a90e2f4e421b96195ed9627159c41
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2018-06-18 12:17:21 +0100

    Remove skip_verification

--- autoscaler/cloudfoundry.py
+++ autoscaler/cloudfoundry.py
@@ -1,12 +1,10 @@
-import os
-
 from cloudfoundry_client.client import CloudFoundryClient
 
 
-def get_client(username, password):
+def get_client(username, password, http_proxy='', https_proxy=''):
     target_endpoint = 'https://api.cloud.service.gov.uk'
-    proxy = dict(http=os.environ.get('HTTP_PROXY', ''), https=os.environ.get('HTTPS_PROXY', ''))
-    client = CloudFoundryClient(target_endpoint, proxy=proxy, skip_verification=True)
+    proxy = dict(http=http_proxy, https=https_proxy)
+    client = CloudFoundryClient(target_endpoint, proxy=proxy)
     client.init_with_user_credentials(username, password)
 
     return client
